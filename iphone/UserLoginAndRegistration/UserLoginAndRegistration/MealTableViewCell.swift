c6d6d1a10a83ef72222343537e95e62989ba5163
xychen0510 <xychen0510@gmail.com>
2016-04-26 17:28:24 -0500

    table view

---
+++ iphone/UserLoginAndRegistration/UserLoginAndRegistration/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  UserLoginAndRegistration
+//
+//  Created by jyao5 on 4/26/16.
+//  Copyright © 2016 food. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
