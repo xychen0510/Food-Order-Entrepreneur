e7c9a8c5c7de387601bb966b8970bd20a6cbe47c
xychen0510 <xychen0510@gmail.com>
2016-04-26 22:24:51 -0500

    tableView_04/27

--- iphone/UserLoginAndRegistration/UserLoginAndRegistration/ViewController.swift
+++ iphone/UserLoginAndRegistration/UserLoginAndRegistration/ViewController.swift
@@ -36,3 +36,28 @@ class ViewController: UIViewController {
 
 }
 
+//
+//  ViewController.swift
+//  Pretty Apple
+//
+//  Created by Duc Tran on 3/28/15.
+//  Copyright (c) 2015 Duc Tran. All rights reserved.
+//
+
+/*import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}*/
+
